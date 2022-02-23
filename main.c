952a66924c9bc4de8a2fb565310dcbc4fab44afd
Lilah254 <98329910+Lilah254@users.noreply.github.com>
2022-02-23 16:57:09 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+A County Library Management System
+Created by Lilian Njeri
+on Jan, 2022
+C89 Compiler
+MIT License
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   printf("\tcounty library\n");
+    printf("Welcome Miss Lilian Njeri. \n");
+    printf("What would you like to do?\n");
+    printf("1. Add new user\n");
+    printf("1. Add Book.\n");
+    printf("Selected action: ");
+    return 0;
+}
